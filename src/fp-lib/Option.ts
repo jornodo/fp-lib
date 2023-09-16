b05ebc5cb286d1bd44a8f70e9fdd175786c3bfd1
Ole Berg <olber@uio.no>
2023-09-17 01:44:32 +0200

    yye

--- src/fp-lib/Option.ts
+++ src/fp-lib/Option.ts
@@ -6,11 +6,14 @@ interface IOption<T> {
     ifPresent: (fn: (value: T) => void) => void; // Evil side effect
     map: (fn: (value: T) => T) => IOption<T>;
     getOrElse: (defaultValue: T) => T;
-    get: () => T;
+    get: () => T | Error | undefined;
     tryCatch: (fn: () => T) => IOption<T>;
 }
 
 
+// Option class
+// A class that wraps a value in an Option
+// Contains methods to apply functions on the wrapped value, handle errors, handle no value and unwrap the value
 export class Option<T> implements IOption<T> {
     readonly value?: T;
     readonly error?: Error;
@@ -27,40 +30,53 @@ export class Option<T> implements IOption<T> {
         this.tryCatch = this.tryCatch.bind(this);
     }
 
+    // Checks if the value is present
+    // @returns {boolean} true if value is present, false otherwise
     isPresent(): boolean {
         return this.value !== undefined;
     }
 
+    // Executes the given function if the value is present
+    // @param {Function} fn - function to be executed if value is present
+    // @returns {void} 
     ifPresent(fn: (value: T) => void): void {
-        if (this.value) {
-            fn(this.value);
-        }
+        this.isPresent() ? fn(<T> this.value) : undefined;
+    }
+    
+
+    // get the value if present
+    // @returns {T} the value if present, undefined otherwise
+    get(): T | Error | undefined {
+        return this.value ? <T> this.value 
+            : this.error ? this.error
+            : <T> undefined;
     }
 
-    get(): T {
-        return <T>this.value;
-    }
-
-    of(value: T): IOption<T> {
-        return new Option(value);
-    }
-
+    // Applies the given function on the current Options value
+    // @param {Function} fn - function to be applied on the current Options value
+    // @returns {Option<T>} an Option with the value returned by the given function applied on the current Options value
     map(fn: (value: T) => T): IOption<T> {
-        if (this.value) {
+        if (this.value !== undefined) {
             return new Option(fn(this.value));
         }
         return new Option(<T> undefined, new Error('No value'));
     }
 
+    // Returns the value if present, the given default value otherwise
+    // @param {T} defaultValue - the value to be returned if no value is present
+    // @returns {T} the value if present, the given default value otherwise
     getOrElse(defaultValue: T): T {
-        if (this.value) {
+        if (this.value !== undefined) {
             return this.value;
         }
         return defaultValue;
     }
 
+    // Applies the given function on the current Options value
+    // @param {Function} fn - function to be applied on the current Options value
+    // @returns {Option<T>} an Option with the value returned by the given function applied on the current Options value
     tryCatch(fn: (t: T) => T): Option<T>{
-        if(!this.value) return new Option(<T>undefined, new Error('No value'));
+        if(this.value === undefined) return new Option(<T>undefined, new Error('No value'));
 
         try {
             return new Option(fn(<T> this.value));
@@ -69,6 +85,9 @@ export class Option<T> implements IOption<T> {
         }
     } 
 
+    // Wraps the given value in an Option
+    // @param {T} value - the value to be wrapped in an Option
+    // @returns {Option<T>} an Option with the given value
     static of<T>(value: T): Option<T> {
         return new Option(value);
     }
--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,7 @@ const l = [1, 2, 3, 4, undefined, 5];
 
 const d = l.map(val => Option.of(val));
 
+
 d.filter((o) => o.isPresent())
   .map(o => o.get())
   .forEach((val) => log(val));
@@ -31,4 +32,20 @@ d.filter((o) => o.isPresent())
 log('------------------')
 
 d.forEach((o) => o.ifPresent((val) => log(val)));
+const t: number[] = [];
+
+log('------------------')
+
+d.forEach((o) => o.ifPresent((val) => t.push(val as number)));
+log(t);
+log('------------------')
+
+ const li = [1, 2, 3, 4, undefined, 5];
+ 
+li.map(o => Option.of(o)).map((o) => o.tryCatch((val) => <number> val + 1))
+.forEach((o) => log(o.get() instanceof Error ? (o.get() as Error).message : o.get()))
+
+log('------------------')
+
+const one = Option.of(1);
 
