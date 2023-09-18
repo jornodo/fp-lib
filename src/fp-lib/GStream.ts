db62459dba39f6906b9b46a9cfdaa847388c7d61
Ole Berg <olber@uio.no>
2023-09-18 19:01:29 +0200

    added infinite streams

--- src/fp-lib/GStream.ts
+++ src/fp-lib/GStream.ts
@@ -11,12 +11,50 @@ export class GStream<T> {
         return new GStream(generator());
     }
 
+    static infinite(): GStream<number> {
+        let i = 1;
+        function* generator() {
+            while(true) {
+                yield i++;
+            }
+        }
+
+        return new GStream(generator());
+    }
+
+    static infiniteFrom(start: number): GStream<number> {
+        function* generator() {
+            while(true) {
+                yield start++;
+            }
+        }
+
+        return new GStream(generator());
+    }
+
+    concat(stream: GStream<T>): GStream<T> {
+        const self = this;
+
+        function* generator() {
+            for(const element of self.generator) {
+                yield element;
+            }
+
+            for(const element of stream.generator) {
+                yield element;
+            }
+        }
+
+        return new GStream(generator());
+
+    }
+
     filter(predicate: (element: T) => boolean): GStream<T> {
         const self = this;
 
         function* generator() {
             for(const element of self.generator) {
-                console.log(`filtering with ${predicate}, and element: ${element}`);
+                
                 if(predicate(element)) {
                     yield element;
                 }
@@ -31,7 +69,7 @@ export class GStream<T> {
 
         function* generator() {
             for(const element of self.generator) {
-                console.log(`mapping with ${mapper}, and element: ${element}`);
+
                 yield mapper(element);
             }
         }
@@ -52,6 +90,18 @@ export class GStream<T> {
 
     getAny(): T | undefined {
         return <T | undefined>this.generator.next().value;
+    }
+
+    
+    // recursively get n elements
+    getNElements(n: number): T[] {
+        const getNElementsHelper = (index: number, acc: T[]): T[] => {
+            return index === n 
+                ? acc
+                : getNElementsHelper(index + 1, [...acc, <T> this.generator.next().value]);
+        }
+
+        return getNElementsHelper(0, []);
     }
         
 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { log } from 'console';
 import { Option } from './fp-lib/Option';
 import { Stream } from './fp-lib/Stream';
 import { GStream } from './fp-lib/GStream';
+import get from 'axios';
 
 console.log('Hello World!');
 
@@ -44,45 +45,54 @@ li.map(val => Option.of(val))
 
   log('------------------')
 
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
 const ls = GStream.of([1, 2, 3, 4, 5, 6, 7, 8]);
 log(
-  ls.filter((val) => val % 2 === 0)
+  ls.map((val) => val + 6)
+    .filter((val) => val % 2 === 0)
     .getAny()
   );
+
+  log('------------------')
+const inf = GStream.infinite();
+log(inf
+    .filter((val) => val % 16  === 0)
+    .map((val) => val * val)
+    .map((val) => val - 420)
+    .getNElements(2)
+);
+
+log('------------------')
+const inf2 = GStream.infiniteFrom(-10);
+log(inf2
+    .filter(i => i > -20)
+    .map(i => i * i)
+    .filter(i => i === 4)
+    .getAny()
+)
+
+log('------------------')
+const ts1 = Stream.of([1, 2, 3]);
+const ts2 = Stream.of([10, 20, 30]);
+
+log(ts1.concat(ts2)
+.filter((val) => val % 2 === 0)
+.toList());
   
+log('------------------')
+
+// Testing api operations
+// const url = 'https://swapi.dev/api/people/';
+
+// const getPeople = async (num: number) => {
+//  return await get(`${url}${num}`);
+// }
+
+// const people = GStream.infinite()
+//   .map(val => val + 2)
+//   .map(async (val) => await getPeople(val).then((res) => res.data.name))
+//   .getNElements(10)
+//   .forEach(async (val) => log(await val));
+
 
 
 
